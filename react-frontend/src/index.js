1ad36c9282f089f0aca76b2f8d62d41851e3e1f1
NguyenTran2002 <nguyen.tran.2002.work@gmail.com>
2024-09-22 21:42:28 -0500

    added mobile version for team route

--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -1,10 +1,13 @@
+// index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import MobileApp from './MobileApp';
 import SevyAI from './SevyAI';
-import SevyAIMobile from './SevyAIMobile'; // Import the new SevyAIMobile component
+import SevyAIMobile from './SevyAIMobile';
+import Team from './team';
+import TeamMobile from './team-mobile'; // Import the mobile version of Team
 import './i18n';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
@@ -16,6 +19,8 @@ ReactDOM.render(
         <Route path="/mobile" element={<MobileApp />} /> {/* Mobile version */}
         <Route path="/sevyai" element={<SevyAI />} /> {/* SEVY AI full screen */}
         <Route path="/sevyai-mobile" element={<SevyAIMobile />} /> {/* SEVY AI mobile version */}
+        <Route path="/our-team" element={<Team />} /> {/* Our Team page */}
+        <Route path="/team-mobile" element={<TeamMobile />} /> {/* Mobile version of Our Team */}
       </Routes>
     </Router>
   </React.StrictMode>,
