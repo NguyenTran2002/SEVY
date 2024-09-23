d3a3e92dfafe2e631c26c11b1e58ee185e6b0549
NguyenTran2002 <nguyen.tran.2002.work@gmail.com>
2024-09-22 22:43:25 -0500

    update all routes to have working our team button

--- react-frontend/src/team-mobile.js
+++ react-frontend/src/team-mobile.js
@@ -20,7 +20,7 @@ const TeamMobile = () => {
                     <div className="navbar-links">
                         <button onClick={() => navigate('/')}>{t('home')}</button>
                         <button onClick={() => navigate('/sevyai')}>{t('sevy_ai')}</button>
-                        <button>{t('our_team')}</button> {/* Already on Our Team page */}
+                        <button onClick={() => navigate('/our-team')}>{t('our_team')}</button> {/* Already on Our Team page */}
                     </div>
                 </div>
                 <div className="navbar-right">
